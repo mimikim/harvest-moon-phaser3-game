fb4bd9f030baaef7589c4d8aa491af1e134fd17a
Mimi Kim <mimikimwebdeveloper@gmail.com>
2020-10-31 00:44:20 -0400

    updating /docs/ directory

--- docs/levels/farm.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="farm" tilewidth="80" tileheight="80" tilecount="360" columns="30">
- <image source="../images/background/farm.png" width="2400" height="960"/>
-</tileset>
