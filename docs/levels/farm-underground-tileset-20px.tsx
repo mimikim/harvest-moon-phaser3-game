af0c287d11b96b83425ac0b2f35d67992061e1ce
Mimi Kim <mimikimwebdeveloper@gmail.com>
2020-11-08 05:13:53 -0500

    pushing to docs

--- docs/levels/farm-underground-tileset-20px.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="farm-underground-tileset-20px" tilewidth="20" tileheight="20" tilecount="11776" columns="64">
- <image source="../images/background/farm-underground.png" width="1280" height="3680"/>
-</tileset>
--- docs/levels/home-tilesheet-20px.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="home" tilewidth="20" tileheight="20" tilecount="5408" columns="104">
- <image source="../images/background/home.png" width="2080" height="1040"/>
-</tileset>
